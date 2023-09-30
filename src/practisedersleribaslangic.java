7135891cfee0c5fe27379e25ad82ee6c66a567af
myusufsever <yusuf-sever@hotmail.com>
2023-09-30 10:31:41 +0300

    first commit

---
+++ src/practisedersleribaslangic.java
@@ -0,0 +1,16 @@
+public class practisedersleribaslangic {
+    public static void main(String[] args) {
+
+        //aynı anda komut islemi yapabilmek adina seksi bir sey bunlari her yerde ogrenemezsin
+        //aynı anda komut islemi yapabilmek adina seksi bir sey bunlari her yerde ogrenemezsin
+        //aynı anda komut islemi yapabilmek adina seksi bir sey bunlari her yerde ogrenemezsin
+
+
+        String name = "Yusuf Sever";
+        System.out.println("name = " + name);
+        System.out.println("hello world \n Hellowise");
+
+
+    }
+}
+
---
+++ src/yeniclassdestek.java
@@ -0,0 +1,5 @@
+public class yeniclassdestek {
+    public static void main(String[] args) {
+        System.out.println("herseydeneme amacli ");
+    }
+}
